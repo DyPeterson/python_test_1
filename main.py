d83eb51b15251dc2c43f04743b4af091cff6a6b4
root <dylan.peterson17@gmail.com>
2022-04-08 13:52:11 -0700

    return sequenced ids for artists to a dictionary

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 class Artists:
-    artist_seq = 100
+    artists_seq = 100
     def __init__(self, name, dob, genre):
         self.name = name
         self.dob = dob
         self.genre = genre
-        self.artist_id = Artists.artist_seq
-        Artists.artist_seq += 1
-
+        self.artist_id = Artists.artists_seq
+        Artists.artists_seq += 1
+    def __repr__(self):
+        return f"Name:{self.name} Dob:{self.dob} Genre: {self.genre} ID:{self.artist_id}"
     def art_print(self):
-        print(f"ID:{self.artist_id}")
+        print(f"Name:{self.name} Dob:{self.dob} ID:{self.artist_id}")
+        return self.artist_id
 
 class Songs:
     def __init__(self, artist, title, length, lyrics):
@@ -17,10 +19,15 @@ class Songs:
         self.length = length
         self.lyrics = lyrics
         # check if self.artist is = Artists.name
-        # print the artists name and say it exists in the database.
+        # if self.artist == Artists.name:
+        #   print(f"{self.artist} has a song in our database!")
+        # # print the artists name and say it exists in the database.
+        # else:
+        #     print(f"Sorry no songs for {self.artist} exists in our database")
 
 
-artist1 = Artists("Bob Dylan", "5-24-1942", "Folk",)
+
+artist1 = Artists("Bob Dylan", "5-24-1942", "Folk")
 artist2 = Artists("Paula Abdul", "6-19-1962", "Pop")
 artist3 = Artists("Cab Calloway", "12-25-1907", "Jazz")
 artist4 = Artists("Jeremy Bolm", "4-06-1983", "Hardcore")
@@ -28,13 +35,18 @@ song1 = Songs("The Times They Are A-Changin'","Bob Dylan","3:12","Come gather 'r
 song2 = Songs("Straight Up","Paula Abdul","4:11","Lost in a dream I don't know which way to go. (A-let me say it.) If you are all that you seem Then, baby, I'm movin' way too slow.")
 song3 = Songs("Mama Tried","Merle Haggard","2:14","The first thing I remember knowing Was a lonesome whistle blowing And a young'un's dream of growing up to ride On a freight train leaving town Not knowing where I'm bound And no one could change my mind but Mama tried")
 artist_dict = {
-    Artists.artist_seq : "Bob Dylan",
-    Artists.artist_seq : "Paula Abdul",
-    Artists.artist_seq : "Cab Calloway",
+    artist1.artist_id : artist1,
+    artist2.artist_id : artist2,
+    artist3.artist_id : artist3,
+    artist4.artist_id : artist4,
 }
-# print(Artists.artist_id)
-print(artist1)
-Artists.art_print(artist1)
-Artists.art_print(artist2)
-Artists.art_print(artist3)
-Artists.art_print(artist4)
+# for artist in artist_dict:
+#     name = artist1.name
+#     dob = artist1.dob
+#     genre = artist1.genre
+    # Artists.art_print(artist)
+print(artist_dict)
+# for key in artist_dict:
+#     artist = Artists(name, dob, genre)
+#     artist_id = f"{key}"
+#     print(key)
