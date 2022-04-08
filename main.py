72b0883be0a086c14a99dcbb981b7f1b59e40717
root <root@DESKTOP-8ASBMTF.localdomain>
2022-04-08 10:20:39 -0700

    add songs and artists to run class tests on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Artists:
-    artist_seq = 10
+    artist_seq = 100
     def __init__(self, name, dob, genre):
         self.name = name
         self.dob = dob
@@ -16,11 +16,17 @@ class Songs:
         self.title = title
         self.length = length
         self.lyrics = lyrics
+        # check if self.artist is = Artists.name
+        # print the artists name and say it exists in the database.
 
 
 artist1 = Artists("Bob Dylan", "5-24-1942", "Folk",)
 artist2 = Artists("Paula Abdul", "6-19-1962", "Pop")
 artist3 = Artists("Cab Calloway", "12-25-1907", "Jazz")
+artist4 = Artists("Jeremy Bolm", "4-06-1983", "Hardcore")
+song1 = Songs("The Times They Are A-Changin'","Bob Dylan","3:12","Come gather 'round people Wherever you roam And admit that the waters Around you have grown And accept it that soon You'll be drenched to the bone If your time to you is worth savin' Then you better start swimmin' Or you'll sink like a stone For the times they are a-changin'")
+song2 = Songs("Straight Up","Paula Abdul","4:11","Lost in a dream I don't know which way to go. (A-let me say it.) If you are all that you seem Then, baby, I'm movin' way too slow.")
+song3 = Songs("Mama Tried","Merle Haggard","2:14","The first thing I remember knowing Was a lonesome whistle blowing And a young'un's dream of growing up to ride On a freight train leaving town Not knowing where I'm bound And no one could change my mind but Mama tried")
 artist_dict = {
     Artists.artist_seq : "Bob Dylan",
     Artists.artist_seq : "Paula Abdul",
@@ -30,3 +36,4 @@ artist_dict = {
 print(artist1)
 Artists.art_print(artist1)
 Artists.art_print(artist2)
+Artists.art_print(artist3)
